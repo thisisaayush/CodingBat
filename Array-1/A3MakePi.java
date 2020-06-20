7c75a69535830d779bc0c75db58eba728de28b31
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-20 16:52:48 -0500

    Update A3MakePi.java

--- Array-1/A3MakePi.java
+++ Array-1/A3MakePi.java
@@ -1,6 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Array1;
+//this code returns the pi value with first two decimal value.
 public class A3MakePi {
     public static int[] makePi(){
         int[] pi = {3,1,4};
