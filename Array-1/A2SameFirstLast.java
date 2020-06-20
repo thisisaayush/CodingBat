aa88240d55462f010a906e162d6356adc86909f1
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-20 16:52:01 -0500

    Update A2SameFirstLast.java

--- Array-1/A2SameFirstLast.java
+++ Array-1/A2SameFirstLast.java
@@ -3,8 +3,9 @@
 package Array1;
 
 import javax.naming.PartialResultException;
-
-public class A2SameFirstLast {
+//this code checks whether the first and the last element of an array is same or not. If it is, it returns true value.
+public class A2SameFirstLast
+{
     public static boolean sameFirstLast(int[] nums){
         if(nums.length < 1){
             return false;
