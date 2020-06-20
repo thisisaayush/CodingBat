fcd3ec1b3d6a95997143e3cf68562e3f25b89eb1
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-20 16:54:11 -0500

    Update A4CommonEnd.java

--- Array-1/A4CommonEnd.java
+++ Array-1/A4CommonEnd.java
@@ -1,6 +1,8 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Array1;
+//this code compares the first elements and the last elements of two int arrays and if it is equals it returns true value otherwise it 
+//returns false value.
 public class A4CommonEnd {
     public static boolean commonEnd(int[] a, int [] b){
         if(a.length < 1 && b.length < 1){
