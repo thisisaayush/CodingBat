e9ccd3ada56ce16ad2f9b2d83196d721c2c4b57f
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-20 16:50:42 -0500

    Update A1FirstLast6.java

--- Array-1/A1FirstLast6.java
+++ Array-1/A1FirstLast6.java
@@ -1,7 +1,10 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+
+//this code checks whether the first and the last element of an array is 6 or not. If it is 6, it returns true value.
 package Array1;
-public class A1FirstLast6 {
+public class A1FirstLast6 
+{
     public static boolean firstLast6(int[] nums){
         if(nums[0] == 6 || nums[nums.length - 1] == 6 ){
             return true;
