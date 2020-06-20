f13d0a51f89cf5b114ac35f9b0f22bc4640c218e
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-20 16:55:19 -0500

    Update A5Sum3.java

--- Array-1/A5Sum3.java
+++ Array-1/A5Sum3.java
@@ -1,6 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Array1;
+//this code returns the sum value of all the int elements of an int array.
 public class A5Sum3 {
     public static int sum3(int[] nums) {
         int sum = 0;
