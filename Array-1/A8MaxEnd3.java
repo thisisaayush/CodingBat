bce63d24a2e6db4ee09a01fc25ab0f84e90262f5
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-23 16:27:54 -0500

    Update A8MaxEnd3.java

--- Array-1/A8MaxEnd3.java
+++ Array-1/A8MaxEnd3.java
@@ -1,5 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This code compares the value between first index of an array and last index of an array, the max value thus obtained 
+//from the comparison is used to replace all the values of an array.
 package Array1;
 public class A8MaxEnd3 {
     public static int[] maxEnd3(int[] nums){
