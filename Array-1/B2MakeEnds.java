de32d638b9447a71a6c8ff43d79d32161f64792d
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-23 16:32:29 -0500

    Update B2MakeEnds.java

--- Array-1/B2MakeEnds.java
+++ Array-1/B2MakeEnds.java
@@ -1,5 +1,6 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This code returns a new array of length 2 containing the first and last elements from the original array.
 package Array1;
 public class B2MakeEnds {
     public int[] makeEnds(int[] nums) {
