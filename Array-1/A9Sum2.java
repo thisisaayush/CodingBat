d0cbded4615265a4e72a740d67f5e4cb83099421
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-23 16:30:00 -0500

    Update A9Sum2.java

--- Array-1/A9Sum2.java
+++ Array-1/A9Sum2.java
@@ -1,5 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This code returns the sum of first two int element of an array. If an array has only one element it returns that element
+//and if an array is empty, it returns 0.
 package Array1;
 public class A9Sum2 {
     public static int sum2(int[] nums){
