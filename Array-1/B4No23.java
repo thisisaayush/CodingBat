891c9a664cae2f3d05000ce980900535e72943db
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-25 20:41:29 -0500

    Update B4No23.java

--- Array-1/B4No23.java
+++ Array-1/B4No23.java
@@ -1,5 +1,6 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//The program return true if an array does not contain element 2 or 3.
 package Array1;
 public class B4No23 {
     public boolean no23(int[] nums) {
