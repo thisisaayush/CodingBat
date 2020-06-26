5bde1a9628a8945bc1f83d4172ad2f2a1e998435
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-25 20:43:44 -0500

    Update B5MakeLast.java

--- Array-1/B5MakeLast.java
+++ Array-1/B5MakeLast.java
@@ -1,5 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program will double the length of an array and replaces all the element of an array to 0 besides the element at last index of an array. 
+//Element at the last index will be same as the original element.
 package Array1;
 public class B5MakeLast {
     public int[] makeLast(int[] nums) {
