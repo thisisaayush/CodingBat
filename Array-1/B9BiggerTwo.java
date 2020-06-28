fd43adbdba38b650fa901b916e8975a29d7fb6e7
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-27 23:11:26 -0500

    Update B9BiggerTwo.java

--- Array-1/B9BiggerTwo.java
+++ Array-1/B9BiggerTwo.java
@@ -1,5 +1,9 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program has 2 arrays of length 2. It will return an array whose sum of elements is the largest.
+//B9BiggerTwo([1, 2], [3, 4]) → [3, 4]
+//B9BiggerTwo([3, 4], [1, 2]) → [3, 4]
+//B9BiggerTwo([1, 1], [1, 2]) → [1, 2]
 package Array1;
 public class B9BiggerTwo {
     public int[] biggerTwo(int[] a, int[] b) {
