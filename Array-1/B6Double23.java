12a817e4aada501fd981a5f943ec92f53b0e11bc
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-27 23:06:14 -0500

    Update B6Double23.java

--- Array-1/B6Double23.java
+++ Array-1/B6Double23.java
@@ -1,5 +1,6 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program return true if an array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2.
 package Array1;
 public class B6Double23 {
     public boolean double23(int[] nums) {
