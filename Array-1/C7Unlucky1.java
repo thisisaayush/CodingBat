2920bdc5ee23f5b4cf2bb00a754e2ab22a9a1d3c
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-28 22:20:14 -0500

    Update C7Unlucky1.java

--- Array-1/C7Unlucky1.java
+++ Array-1/C7Unlucky1.java
@@ -1,5 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1. Return true if
+//the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
 package Array1;
 public class C7Unlucky1 {
     public boolean unlucky1(int[] nums) {
