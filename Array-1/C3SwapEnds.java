ca8d36aa6fbf57892e36fec130635b5643db2bf3
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-28 22:16:05 -0500

    Update C3SwapEnds.java

--- Array-1/C3SwapEnds.java
+++ Array-1/C3SwapEnds.java
@@ -1,5 +1,6 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program has an array of ints, swap the first and last elements in the array. Return the modified array. The array length will be at least 1.
 package Array1;
 public class C3SwapEnds {
     public int[] swapEnds(int[] nums) {
