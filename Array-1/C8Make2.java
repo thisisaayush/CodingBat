eb874c9a0aa9fdca6f2ad73d26b5e2c93edd59a7
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-28 22:19:31 -0500

    Update C8Make2.java

--- Array-1/C8Make2.java
+++ Array-1/C8Make2.java
@@ -1,5 +1,10 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+/*
+This program has 2 int arrays, a and b, return a new array length 2 containing, as much as will fit, 
+the elements from a followed by the elements from b. The arrays may be any length, including 0,
+but there will be 2 or more elements available between the 2 arrays.
+*/
 package Array1;
 public class C8Make2 {
     public int[] make2(int[] a, int[] b) {
