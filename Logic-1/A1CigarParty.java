9a47a873082971c44e05f14c04e1d35e74db7a1b
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-06-30 21:55:29 -0500

    Update A1CigarParty.java

--- Logic-1/A1CigarParty.java
+++ Logic-1/A1CigarParty.java
@@ -1,5 +1,10 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+/*
+This program is about a squirrel party. It is successful when the number of cigars is between 40 and 60, inclusive. Unless 
+it is the weekend, in which case there is no upper bound on the number of cigars. Return true if the party
+with the given values is successful, or false otherwise.
+*/
 package Logic1;
 public class A1CigarParty {
     public static boolean cigarParty(int cigars, boolean isWeekend){
