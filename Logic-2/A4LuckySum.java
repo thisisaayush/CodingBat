99f89fb9868ad36ed5a4057ffb0e6e5e48423340
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-01 22:03:21 -0500

    Update A4LuckySum.java

--- Logic-2/A4LuckySum.java
+++ Logic-2/A4LuckySum.java
@@ -1,5 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program is about 3 int values, a b c, which returns their sum. However, if one of the values is 13 then it does not
+//count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count.
 package Logic2;
 public class A4LuckySum {
     public int luckySum(int a, int b, int c) {
