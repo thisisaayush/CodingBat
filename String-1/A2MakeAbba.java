d787e5ac2be946ab2bd56856b4c85b916a0946fc
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-06 22:18:22 -0500

    Update A2MakeAbba.java

--- String-1/A2MakeAbba.java
+++ String-1/A2MakeAbba.java
@@ -1,5 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program has makeAbba method that has two parameters and prints out the 
+//parameter in order of string a + string b + string b + string a.
 package String1;
 public class A2MakeAbba {
     public static String makeAbba(String a, String b){
