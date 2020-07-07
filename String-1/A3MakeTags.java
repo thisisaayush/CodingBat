ad18cf592cc64dc396066ceeec8bba1fca2af37a
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-06 22:20:28 -0500

    Update A3MakeTags.java

--- String-1/A3MakeTags.java
+++ String-1/A3MakeTags.java
@@ -1,5 +1,9 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program is simple demonstration of HTML tags, it prints out the entered tag parameter and word parameter in the following
+//formate:
+//makeTags("i", "Yay") → "<i>Yay</i>"
+//makeTags("i", "Hello") → "<i>Hello</i>"
 package String1;
 public class A3MakeTags {
     public static String makeTags(String tag, String word){
