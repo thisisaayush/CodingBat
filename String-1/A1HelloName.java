8bf456ca33c75c3f19614e68d25619b582df013f
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-06 22:17:05 -0500

    Update A1HelloName.java

--- String-1/A1HelloName.java
+++ String-1/A1HelloName.java
@@ -1,5 +1,6 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program just prints out Hello + whatever string is entered.
 package String1;
 public class A1HelloName {
     public static String helloName(String str){
