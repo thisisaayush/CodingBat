b47e2bdcdfe0969c4886dd0181bf2fd75dc267bc
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-10 22:51:00 -0500

    Update A1StringTimes.java

--- Warmup-2/A1StringTimes.java
+++ Warmup-2/A1StringTimes.java
@@ -1,5 +1,9 @@
 //Author: Aayush Shahi Thakuri
 //University: Minnesota State University, Mankato
+//This program has method stringTimes with two parameters string and int. It return a larger string that is n copies of the original string.
+//For instance:
+//stringTimes("Hi", 2) → "HiHi"
+//stringTimes("Hi", 3) → "HiHiHi"
 package WarmUp2;
 public class A1StringTimes {
     public static String stringTimes(String str, int n){
