7f6375e052af2d7e9c083ddf4042c60f28c296f9
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-10 22:53:12 -0500

    Update A2FrontTimes.java

--- Warmup-2/A2FrontTimes.java
+++ Warmup-2/A2FrontTimes.java
@@ -1,5 +1,10 @@
-//Author: Aayush Shahi Thakuri
-//University: Minnesota State University, Mankato
+/*Author: Aayush Shahi Thakuri
+University: Minnesota State University, Mankato
+This program has fronTimes method with parameters string and int. We'll say that the front of the string
+is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front.
+For instance: 
+frontTimes("Chocolate", 2) → "ChoCho"
+frontTimes("Chocolate", 3) → "ChoChoCho"*/
 package WarmUp2;
 public class A2FrontTimes {
     public static String frontTimes(String str, int n){
