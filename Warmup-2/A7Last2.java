8be0ddc48cb309f4c461546b937134ea01a615a1
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-13 21:55:26 -0500

    Update A7Last2.java

--- Warmup-2/A7Last2.java
+++ Warmup-2/A7Last2.java
@@ -1,5 +1,12 @@
 //Author: Aayush Shahi Thakuri
 //University: Minnesota State University, Mankato
+/*
+This program returns the count of the number of times that a substring length 2 appears in the string and also as the last 2 
+chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+last2("hixxhi") → 1
+last2("xaxxaxaxx") → 1
+*/
+
 package WarmUp2;
 public class A7Last2 {
     public static int last2(String str){
