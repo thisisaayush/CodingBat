c98f860ed40933e79fe830768cae1a32d763e789
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-13 21:56:29 -0500

    Update A8ArrayCount9.java

--- Warmup-2/A8ArrayCount9.java
+++ Warmup-2/A8ArrayCount9.java
@@ -1,5 +1,10 @@
 //Author: Aayush Shahi Thakuri
 //University: Minnesota State University, Mankato
+/*
+This program returns the number of 9's in the array.
+arrayCount9([1, 2, 9]) → 1
+arrayCount9([1, 9, 9]) → 2
+*/
 package WarmUp2;
 public class A8ArrayCount9 {
     public static int arrayCount9(int[] nums){
