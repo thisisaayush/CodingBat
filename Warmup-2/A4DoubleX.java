6cd73d618ed5720d7ecbedef8545d78a6528efb6
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-13 21:52:41 -0500

    Update A4DoubleX.java

--- Warmup-2/A4DoubleX.java
+++ Warmup-2/A4DoubleX.java
@@ -1,5 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //University: Minnesota State University, Mankato
+//This program returns true if the first instance of "x" in the string is immediately followed by another "x".
+
 package WarmUp2;
 public class A4DoubleX {
     public static boolean doubleX(String str){
