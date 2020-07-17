ab6e94ad88404b57cda978eab2124f9446373eb4
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-17 15:41:56 -0500

    Update B9Has271.java

--- Warmup-2/B9Has271.java
+++ Warmup-2/B9Has271.java
@@ -1,5 +1,9 @@
 //Author: Aayush Shahi Thakuri
 //University: Minnesota State University, Mankato
+//This program has an array of ints,and it return true if it contains a 2, 7, 1 pattern: a value, followed by the value plus 5, 
+//followed by the value minus 1. Additionally the 271 counts even if the "1" differs by 2 or less from the correct value. For instance:
+//has271([1, 2, 7, 1]) → true
+//has271([1, 2, 8, 1]) → false
 package WarmUp2;
 public class B9Has271 {
     public static boolean has271(int nums[]){
