d1bcc34f226bf30705e5dfae57edf38957e37363
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-17 15:42:48 -0500

    Update B8NoTriples.java

--- Warmup-2/B8NoTriples.java
+++ Warmup-2/B8NoTriples.java
@@ -1,5 +1,9 @@
 //Author: Aayush Shahi Thakuri
 //University: Minnesota State University, Mankato
+//This program has  an array of ints, we'll say that a triple is a value appearing 3 times 
+//in a row in the array. Return true if the array does not contain any triples. For instance:
+//noTriples([1, 1, 2, 2, 1]) → true
+//noTriples([1, 1, 2, 2, 2, 1]) → false
 package WarmUp2;
 public class B8NoTriples {
     public static boolean noTriplse(int[] nums){
