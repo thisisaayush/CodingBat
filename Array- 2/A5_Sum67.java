711a64ffd4cdafd5927daaf5019ed68cd05ada09
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-19 10:50:35 -0500

    Add files via upload

---
+++ Array- 2/A5_Sum67.java
@@ -0,0 +1,23 @@
+package Array2;
+
+public class A5_Sum67 {
+    public static void main(String[] args) {
+        int array[] = {1,5,6,3,7,9};
+        System.out.println("Sum67: " + sum67( array ));
+    }
+    public static int sum67(int[] array){
+        int sum = 0;
+
+        for(int i = 0 ; i < array.length ; i++){
+            if(array[i] == 6){
+                while (array[i] != 7){
+                    i++;
+                }
+            }
+            else {
+                sum = sum + array[i];
+            }
+        }
+        return sum;
+    }
+}
