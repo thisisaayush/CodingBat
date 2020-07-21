44713f9b94951ceda19ae10fcdcfc7598c94febe
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-20 21:05:32 -0500

    Update A5_Sum67.java

--- Array- 2/A5_Sum67.java
+++ Array- 2/A5_Sum67.java
@@ -1,5 +1,5 @@
 package Array2;
-
+//This program returns the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7.
 public class A5_Sum67 {
     public static void main(String[] args) {
         int array[] = {1,5,6,3,7,9};
