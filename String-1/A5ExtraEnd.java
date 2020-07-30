1073b673b7dfb2e02adb5d563e6809b4fbad9849
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-29 22:51:03 -0500

    Update A5ExtraEnd.java

--- String-1/A5ExtraEnd.java
+++ String-1/A5ExtraEnd.java
@@ -1,5 +1,6 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program  returns a new string made of 3 copies of the last 2 chars of the original string.
 package String1;
 public class A5ExtraEnd {
     public static String extraEnd(String str){
