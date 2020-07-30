2459ab41e5dd1b03819029ac986379ed99df1ef7
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-29 22:54:45 -0500

    Update A8WithoutEnd.java

--- String-1/A8WithoutEnd.java
+++ String-1/A8WithoutEnd.java
@@ -1,5 +1,6 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program has a string, it returns a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
 package String1;
 public class A8WithoutEnd {
     public static String withoutEnd(String str){
