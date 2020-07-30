d058b1defa06086b8c94224f06a1a18f715d4204
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-29 22:51:47 -0500

    Update A6FirstTwo.java

--- String-1/A6FirstTwo.java
+++ String-1/A6FirstTwo.java
@@ -1,5 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program returns the string made of its first two chars, so the String "Hello" yields "He". 
+//If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
 package String1;
 public class A6FirstTwo {
     public static String firstTwo(String str){
