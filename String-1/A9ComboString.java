77aded47d264ac0042650f466c8b5f4c6e2df4af
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-29 22:56:10 -0500

    Update A9ComboString.java

--- String-1/A9ComboString.java
+++ String-1/A9ComboString.java
@@ -1,5 +1,9 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program has 2 strings, a and b, return a string of the form short+long+short, with the shorter string on 
+//the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0). For example:
+//comboString("Hello", "hi") → "hiHellohi"
+//comboString("hi", "Hello") → "hiHellohi"
 package String1;
 public class A9ComboString {
     public static String comboString(String a, String b){
