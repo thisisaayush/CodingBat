d8fa213f15619ed60fe11dda7360528d6a4da1c1
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-29 22:52:35 -0500

    Update A7FirstHalf.java

--- String-1/A7FirstHalf.java
+++ String-1/A7FirstHalf.java
@@ -1,5 +1,6 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program has a string of even length, it returns the first half. So the string "WooHoo" yields "Woo".
 package String1;
 public class A7FirstHalf {
     public static String firstHalf(String str){
