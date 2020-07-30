6d7b3d1bfa601a1f8633fe6f4196b11a68963ea9
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-07-29 22:50:25 -0500

    Update A4MakeOutWord.java

--- String-1/A4MakeOutWord.java
+++ String-1/A4MakeOutWord.java
@@ -1,5 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//This program for instance returns an "out" string length 4, such as "<<>>", and a word, a new
+//string where the word is in the middle of the out string, e.g. "<<word>>"
 package String1;
 public class A4MakeOutWord {
     public static String makeOutWord(String out, String word){
