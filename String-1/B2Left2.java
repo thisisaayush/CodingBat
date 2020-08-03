f7777d6c6f891beca66ab31b840115cc971f75ea
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-02 22:44:04 -0500

    Update B2Left2.java

--- String-1/B2Left2.java
+++ String-1/B2Left2.java
@@ -1,6 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package String1;
+//The program has a string, it returns a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
 public class B2Left2 {
     public static String left2(String str){
         if(str.length() < 2){
