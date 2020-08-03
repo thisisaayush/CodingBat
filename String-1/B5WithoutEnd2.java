7fe3f58a5ffff2f428715031218e3fc71311f9eb
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-02 22:47:00 -0500

    Update B5WithoutEnd2.java

--- String-1/B5WithoutEnd2.java
+++ String-1/B5WithoutEnd2.java
@@ -1,6 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package String1;
+//The program has a string, it returns a version without both the first and last char of the string. The string may be any length, including 0.
 public class B5WithoutEnd2 {
     public static String withoutEnd2(String str){
         if(str.length() < 2){
