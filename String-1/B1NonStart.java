c34c37e74f847abf57edaefb009886abd4405f72
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-02 22:43:29 -0500

    Update B1NonStart.java

--- String-1/B1NonStart.java
+++ String-1/B1NonStart.java
@@ -1,6 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package String1;
+//The program has two strings, it  returns their concatenation, except omit the first char of each. The strings will be at least length 1.
 public class B1NonStart {
     public static String nonStart(String a, String b){
         if(a.length() < 1 && b.length() < 1){
