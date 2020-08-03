96137be676e439331f85fecdfbfbab1bb8e299d0
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-02 22:46:14 -0500

    Update B4TheEnd.java

--- String-1/B4TheEnd.java
+++ String-1/B4TheEnd.java
@@ -1,6 +1,8 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package String1;
+//The program has  a string, it returns a string length 1 from its front, unless front is false, in which case 
+//return a string length 1 from its back. The string will be non-empty.
 public class B4TheEnd {
     public static String theEnd(String str, boolean front){
         if(front){
