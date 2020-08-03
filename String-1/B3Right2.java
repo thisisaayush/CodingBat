1eaa966fbe789a082fd732be0b55cb4161568f83
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-02 22:44:58 -0500

    Update B3Right2.java

--- String-1/B3Right2.java
+++ String-1/B3Right2.java
@@ -1,6 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package String1;
+//The program has  a string, it returns a "rotated right 2" version where the last 2 chars are moved to the start. The string length will be at least 2.
 public class B3Right2 {
     public static String right2(String str){
         if(str.length() < 2){
