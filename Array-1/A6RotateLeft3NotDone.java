a898e8828dc59cd9b7b88532ab2b9d4216ae971d
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-02 22:48:26 -0500

    Update A6RotateLeft3NotDone.java

--- Array-1/A6RotateLeft3NotDone.java
+++ Array-1/A6RotateLeft3NotDone.java
@@ -1,6 +1,10 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Array1;
-public class A6RotateLeft3NotDone {
 
+public class A6_RotateLeft3 {
+    public int[] rotateLeft3(int[] nums) {
+        int[] x = {nums[1], nums[2],nums[0]};
+        return x;
+    }
 }
