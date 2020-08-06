57125d9f91bef7426bea6940f94b90312b155e53
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-06 13:57:12 -0500

    Update B7EndsLy.java

--- String-1/B7EndsLy.java
+++ String-1/B7EndsLy.java
@@ -1,6 +1,10 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package String1;
+//This program has  a string, return true if it ends in "ly".
+//endsLy("oddly") → true
+//endsLy("y") → false
+//endsLy("oddy") → false
 public class B7EndsLy {
     public static boolean endsLy(String str){
         return str.endsWith("ly");
