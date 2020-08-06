959956f762dde6ddd362017fde464d54cc5dea7d
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-06 13:56:19 -0500

    Update B6Middle2.java

--- String-1/B6Middle2.java
+++ String-1/B6Middle2.java
@@ -1,6 +1,10 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package String1;
+//This program has a string of even length, return a string made of the middle two chars,
+//so the string "string" yields "ri". The string length will be at least 2.
+//middleTwo("string") → "ri"
+//middleTwo("code") → "od"
 public class B6Middle2 {
     public static String middle2(String str){
         if(str.length() % 2 == 0){
