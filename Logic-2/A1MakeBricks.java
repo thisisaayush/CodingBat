76cfd168ea067a79724ea18eb53379cff761b8b1
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-06 13:54:05 -0500

    Update A1MakeBricks.java

--- Logic-2/A1MakeBricks.java
+++ Logic-2/A1MakeBricks.java
@@ -1,6 +1,11 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Logic2;
+//Here, you have to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) 
+//and big bricks (5 inches each). Return true if it is possible to make the goal by choosing from the given bricks.
+//Example:
+//makeBricks(3, 1, 8) → true
+//makeBricks(3, 1, 9) → false
 public class A1MakeBricks {
     public boolean makeBricks(int small, int big, int goal) {
                 if(goal > 5 * big + small){
