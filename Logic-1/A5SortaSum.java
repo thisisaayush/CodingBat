bc79abd8f08f92413e46726ed03611caecd96070
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-12 20:53:25 -0500

    Update A5SortaSum.java

--- Logic-1/A5SortaSum.java
+++ Logic-1/A5SortaSum.java
@@ -1,6 +1,10 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Logic1;
+//The program has  2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+//sortaSum(3, 4) → 7
+//sortaSum(9, 4) → 20
+
 public class A5SortaSum {
     public int sortaSum(int a, int b) {
             int sum = a + b;
