a20c20e194d41505e5fae7dc8ddf7d098b85ebb1
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-14 11:27:32 -0500

    Update B3Less20.java

--- Logic-1/B3Less20.java
+++ Logic-1/B3Less20.java
@@ -1,6 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Logic1;
+//The program returns true if the given non-negative number is 1 or 2 less than a multiple of 20. So for example 38 and 39 return true, but 40 returns false.
 public class B3Less20 {
     public boolean less20(int n) {
         int mult1 = (n + 1) % 20;
