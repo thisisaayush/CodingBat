08a0bc506a30dde911a75ea6e08e1628534c9b20
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-14 11:26:00 -0500

    Update B1More20.java

--- Logic-1/B1More20.java
+++ Logic-1/B1More20.java
@@ -1,6 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Logic1;
+//The program returns true if the given non-negative number is 1 or 2 more than a multiple of 20.
 public class B1More20 {
     public boolean more20(int n) {
             int mult = n % 20;
