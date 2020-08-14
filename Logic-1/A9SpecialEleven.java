f7f713289e7f8afe4b5e817ac3e5bb3fc45b6de1
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-14 11:25:25 -0500

    Update A9SpecialEleven.java

--- Logic-1/A9SpecialEleven.java
+++ Logic-1/A9SpecialEleven.java
@@ -1,6 +1,8 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Logic1;
+//The program returns true if the given non-negative number is special.
+//We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11.
 public class A9SpecialEleven {
     public boolean specialEleven(int n) {
         int mult = n % 11;
