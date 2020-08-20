4d0c2a8572aabbfe8faec34e351491be6e909b8e
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-20 13:52:38 -0500

    Update B5TeenSum.java

--- Logic-1/B5TeenSum.java
+++ Logic-1/B5TeenSum.java
@@ -1,6 +1,8 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Logic1;
+//The program has 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, 
+//are extra lucky. So if either value is a teen, just return 19.
 public class B5TeenSum {
     public int teenSum(int a, int b) {
         int sum = a + b;
