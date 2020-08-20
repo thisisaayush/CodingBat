19bc9d3283112b18460da072fea2fb38675bba0b
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-20 13:52:00 -0500

    Update B4NearTen.java

--- Logic-1/B4NearTen.java
+++ Logic-1/B4NearTen.java
@@ -1,6 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
 package Logic1;
+//The program has a a non-negative number "num", return true if num is within 2 of a multiple of 10.
 public class B4NearTen {
     public boolean nearTen(int num) {
            int remainder = num % 10;
