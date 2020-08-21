62e370d24425e5512e23e1cca41230cb42ba164a
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-21 06:32:17 -0500

    Update B9FizzString2.java

--- Logic-1/B9FizzString2.java
+++ Logic-1/B9FizzString2.java
@@ -1,5 +1,11 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+/*
+The program has an int n, return the string form of the number followed by "!". 
+So the int 6 yields "6!". Except if the number is divisible by 3 use "Fizz" 
+instead of the number, and if the number is divisible by 5 use "Buzz", and if 
+divisible by both 3 and 5, use "FizzBuzz". 
+*/
 package Logic1;
 public class B9FizzString2 {
     public String fizzString2(int n) {
