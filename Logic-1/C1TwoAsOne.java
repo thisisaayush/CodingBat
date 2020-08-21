1f5626111908e54b5ec8e70091ddca09bb16486f
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-21 06:33:01 -0500

    Update C1TwoAsOne.java

--- Logic-1/C1TwoAsOne.java
+++ Logic-1/C1TwoAsOne.java
@@ -1,5 +1,10 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+/*
+The program has three ints, a b c, return true if it is possible to add two of the ints to get the third.
+twoAsOne(1, 2, 3) → true
+twoAsOne(3, 1, 2) → true
+*/
 package Logic1;
 public class C1TwoAsOne {
     public boolean twoAsOne(int a, int b, int c) {
