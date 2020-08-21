d5460d3271a4a47696fcbaef24f51c0fe97ac7f1
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-21 06:34:45 -0500

    Update C2InOrder.java

--- Logic-1/C2InOrder.java
+++ Logic-1/C2InOrder.java
@@ -1,5 +1,11 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+/*
+The program has three ints, a b c, return true if b is greater than a, 
+and c is greater than b. However, with the exception that if "bOk" is true, b does not need to be greater than a.
+inOrder(1, 2, 4, false) → true
+inOrder(1, 2, 1, false) → false
+*/
 package Logic1;
 public class C2InOrder {
     public boolean inOrder(int a, int b, int c, boolean bOk) {
