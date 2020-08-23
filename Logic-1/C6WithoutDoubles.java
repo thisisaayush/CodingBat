0d8481b36f80363d503baaea76c35a72e5cd85a6
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-22 23:56:52 -0500

    Update C6WithoutDoubles.java

--- Logic-1/C6WithoutDoubles.java
+++ Logic-1/C6WithoutDoubles.java
@@ -1,5 +1,8 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//The program returns the sum of two 6-sided dice rolls, each in the range 1..6. 
+//However, if noDoubles is true, if the two dice show the same value, increment 
+//one die to the next value, wrapping around to 1 if its value was 6.
 package Logic1;
 public class C6WithoutDoubles {
     public int withoutDoubles(int die1, int die2, boolean noDoubles) {
