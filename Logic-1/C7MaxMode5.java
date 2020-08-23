6d4448ee4598e4e56e7b691d3fd7e144491d6d1e
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-22 23:57:38 -0500

    Update C7MaxMode5.java

--- Logic-1/C7MaxMode5.java
+++ Logic-1/C7MaxMode5.java
@@ -1,5 +1,8 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//The program has two int values, return whichever value is larger.
+//However if the two values have the same remainder when divided by 5,
+//then the return the smaller value. However, in all cases, if the two values are the same, return 0.
 package Logic1;
 public class C7MaxMode5 {
     public int maxMod5(int a, int b) {
