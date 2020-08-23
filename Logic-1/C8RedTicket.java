06ecf3f9f8526dbd92bfe499b3b5f3cddea7e20f
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-23 00:01:23 -0500

    Update C8RedTicket.java

--- Logic-1/C8RedTicket.java
+++ Logic-1/C8RedTicket.java
@@ -1,5 +1,9 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//The program has a red lottery ticket showing ints a, b, and c, 
+//each of which is 0, 1, or 2. If they are all the value 2, the 
+//result is 10. Otherwise if they are all the same, the result is 5. 
+//Otherwise so long as both b and c are different from a, the result is 1. Otherwise the result is 0.
 package Logic1;
 public class C8RedTicket {
     public int redTicket(int a, int b, int c) {
