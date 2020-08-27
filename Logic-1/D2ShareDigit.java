5a3683fd0902986500859049fc616ff5f232051f
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-27 00:04:58 -0500

    Update D2ShareDigit.java

--- Logic-1/D2ShareDigit.java
+++ Logic-1/D2ShareDigit.java
@@ -1,5 +1,6 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//The program has two ints, each in the range 10..99, return true if there is a digit that appears in both numbers, such as the 2 in 12 and 23. 
 package Logic1;
 public class D2ShareDigit {
     public boolean shareDigit(int a, int b) {
