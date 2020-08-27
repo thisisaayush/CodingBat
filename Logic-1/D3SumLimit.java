bf6864ae13d850cb40ab8ca12be4e7438d755865
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-08-27 00:05:31 -0500

    Update D3SumLimit.java

--- Logic-1/D3SumLimit.java
+++ Logic-1/D3SumLimit.java
@@ -1,5 +1,7 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//The program has 2 non-negative ints, a and b, return their sum, so long 
+//as the sum has the same number of digits as a. If the sum has more digits than a, just return a without b. 
 package Logic1;
 public class D3SumLimit {
     public int sumLimit(int a, int b) {
