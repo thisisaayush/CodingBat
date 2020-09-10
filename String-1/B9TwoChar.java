849330fd58ddedfd132281fcdb1049d5028e9b7e
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-09-10 06:38:37 -0500

    Update B9TwoChar.java

--- String-1/B9TwoChar.java
+++ String-1/B9TwoChar.java
@@ -1,6 +1,9 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
-//The program 
+//The program has a string and an index, return a string length 2 starting at the given index. 
+//If the index is too big or too small to define a string length 2, use the first 2 chars. The string length will be at least 2.
+//twoChar("java", 0) → "ja"
+//twoChar("java", 2) → "va"
 package String1;
 public class B9TwoChar {
     public static String twoChar(String str, int index){
