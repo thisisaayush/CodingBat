4282ee7f072a5b676bdc32e797a413b28d3d1ec9
Aayush Shahi Thakuri <39867316+thisisaayush@users.noreply.github.com>
2020-09-10 06:37:07 -0500

    Update B8NTwice.java

--- String-1/B8NTwice.java
+++ String-1/B8NTwice.java
@@ -1,5 +1,8 @@
 //Author: Aayush Shahi Thakuri
 //Universtiy: Minnesota State University, Mankato
+//The program has a string and an int n, return a string made of the first and last n chars from the string. The string length will be at least n.
+//nTwice("Hello", 2) → "Helo"
+//nTwice("Chocolate", 3) → "Choate"
 package String1;
 public class B8NTwice {
     public static String nTwice(String str, int n){
